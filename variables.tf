e10ded3bffed77aaecd3742587c7ebf2b9f142e9
Tejas Indulkar <tindulkar@slb.com>
2020-02-20 16:32:16 +0530

    mod

--- variables.tf
+++ variables.tf
@@ -4,26 +4,29 @@ variable "subnetname" {}
 variable "csv_input_filename" {}
 variable "env_type" {
     type = map
-    default       = "development"
+default = {
     development   = "development"
     staging       = "staging"
     production    = "production"
+}
   }
+
   env  = "${lookup(local.env_type, "development")}"
 variable "subnetname_env" {
     type = map
-    default       = "subnet-development"
+default = {
     development   = "subnet-development"
     staging       = "subnet-staging"
     production    = "subnet-production"
+}
   }
   subnetname = "${lookup(local.subnetname_env, local.env)}"
 variable "csv_input_filename_env" {
     type = map
-    default       = "./development/gcp_server_input.csv"
+default = {
     development   = "./development/gcp_server_input.csv"
     staging       = "./staging/gcp_server_input.csv"
     production    = "./production/gcp_server_input.csv"
-
+    }
   }
   csv_input_filename = "${lookup(local.csv_input_filename_env, local.env)}"
