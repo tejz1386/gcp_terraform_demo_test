da62decae2a0315eae7b7fb2d0cee2af7a2ae2f2
Tejas Indulkar <tindulkar@slb.com>
2020-02-21 12:53:02 +0530

    correcting main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "app_vm_build" {
   source = "./modules/app_vm_build"
-  csv_input_filename = "./development/gcp_server_input.csv"
+  csv_input_file_name = "./development/gcp_server_input.csv"
   subnetwork = "subnet-development"
 }
