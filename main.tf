5f4c48350553dcb5d1ffa4c73f8f61dc32aa1be2
Tejas Indulkar <tindulkar@slb.com>
2020-02-24 19:15:30 +0530

    correcting names in main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "app_vm_build" {
+module "app_vm_build_3_disks" {
   source = "./modules/app_vm_build_3_disks"
   csv_input_file_name = "./development/gcp_server_input_3_disks.csv"
   subnetwork = "subnet-development"
 }
-module "app_vm_build" {
+module "app_vm_build_4_disks" {
   source = "./modules/app_vm_build_4_disks"
   csv_input_file_name = "./development/gcp_server_input_4_disks.csv"
   subnetwork = "subnet-development"
