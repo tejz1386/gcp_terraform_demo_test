f59988bea0280b87a06d590da557258535c16302
Tejas Indulkar <tindulkar@slb.com>
2020-06-30 19:57:04 +0530

    removing unwanted clutter

--- modules/app_vm_build_3_disks/variables.tf
+++
@@ -1,2 +0,0 @@
-variable  "csv_input_file_name" {}
-variable  "subnetwork" {}
--- modules/disk_creation_2/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "csv_input_file_name" {}
-variable "server_size" {}
-variable "disk_number" {}
-variable "disk_type" {}
