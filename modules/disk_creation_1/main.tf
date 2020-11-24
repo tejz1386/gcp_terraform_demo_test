f77f11a30098bc085a4aee2ee87575e1f8c300b3
Tejas Indulkar <tindulkar@slb.com>
2020-11-24 15:24:01 +0530

    retesting

--- modules/disk_creation_1/main.tf
+++ modules/disk_creation_1/main.tf
@@ -9,9 +9,9 @@ resource "google_compute_disk" "testdisk" {
     }
     physical_block_size_bytes = 4096
 }
-# resource "google_compute_attached_disk" "attachdisk" {
-#   for_each      = var.server_size
-#     disk     =  google_compute_disk.testdisk[each.key].self_link
-#     instance = each.key
-#     zone = "us-west2-a"
-# }
+resource "google_compute_attached_disk" "attachdisk" {
+  for_each      = var.server_size
+    disk     =  google_compute_disk.testdisk[each.key].self_link
+    instance = each.key
+    zone = "us-west2-a"
+}
