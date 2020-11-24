6ee30706d958b2eef9db59d881ab479ef30ba3c0
Tejas Indulkar <tindulkar@slb.com>
2020-11-24 14:22:52 +0530

    disabling disk creation

--- modules/app_vm_build_test/main.tf
+++ modules/app_vm_build_test/main.tf
@@ -11,17 +11,17 @@ module "vm_creation_module_csv" {
   csv_input_file_name = "${var.csv_input_file_name}"
   # subnetwork          = "${var.subnetwork}"
 }
-module "disk_creation_1" {
-  source = "../disk_creation_1"
-  server_size =  module.csv_output_2.server_disk1_consolidation
-  disk_number = "datadisk-d"
-  # disk_type   = "pd-ssd"
-}
-module "disk_creation_2" {
-  source = "../disk_creation_1"
-  server_size =  module.csv_output_2.server_disk2_consolidation
-  disk_number = "pagedisk-e"
-  # disk_type   = "pd-ssd"
+# module "disk_creation_1" {
+#   source = "../disk_creation_1"
+#   server_size =  module.csv_output_2.server_disk1_consolidation
+#   disk_number = "datadisk-d"
+#   # disk_type   = "pd-ssd"
+# }
+# module "disk_creation_2" {
+#   source = "../disk_creation_1"
+#   server_size =  module.csv_output_2.server_disk2_consolidation
+#   disk_number = "pagedisk-e"
+#   # disk_type   = "pd-ssd"
 }
 # module "network_creation" {
 #   source = "../network_creation"
