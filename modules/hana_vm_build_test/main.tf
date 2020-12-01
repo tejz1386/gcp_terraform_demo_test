5b98ae28eaee9014e45814045ef513b3b21644be
tejas <tejz1386@gmail.com>
2020-12-01 14:30:08 +0530

    extra disk additions for hana module

--- modules/hana_vm_build_test/main.tf
+++ modules/hana_vm_build_test/main.tf
@@ -12,15 +12,15 @@ module "vm_creation_module_csv" {
   os_image = "${var.os_image}"
   # subnetwork          = "${var.subnetwork}"
 }
-# module "disk_creation_1" {
-#   source = "../disk_creation_1"
-#   server_size =  module.csv_output_2.server_disk1_consolidation
-#   disk_number = "datadisk-d"
-#   # disk_type   = "pd-ssd"
-# }
-# module "disk_creation_2" {
-#   source = "../disk_creation_1"
-#   server_size =  module.csv_output_2.server_disk2_consolidation
-#   disk_number = "pagedisk-e"
-#   # disk_type   = "pd-ssd"
-# }
+ module "disk_creation_1" {
+   source = "../disk_creation_1"
+   server_size =  module.csv_output_2.server_disk1_consolidation
+   disk_number = "datadisk-hana"
+   # disk_type   = "pd-ssd"
+ }
+ module "disk_creation_2" {
+   source = "../disk_creation_1"
+   server_size =  module.csv_output_2.server_disk2_consolidation
+   disk_number = "datadisk-backup"
+   # disk_type   = "pd-ssd"
+ }
