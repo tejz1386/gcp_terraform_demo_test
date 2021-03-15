e3d2631b97010bf49a7e699aee101e854710b68b
tejas <tejz1386@gmail.com>
2021-03-15 22:05:57 +0530

    testing published module

--- main.tf
+++ main.tf
@@ -23,9 +23,14 @@
 #   # subnetwork = "subnet-development"
 # }
 
-module "module_1" {
-  source  = "app.terraform.io/bso-cloud-03/module_1/google"
+module "server-csv-module" {
+  source  = "app.terraform.io/bso-cloud-03/server-csv-module/google"
   version = "1.0.0"
-  csv_input_storage_file_name = "./development/gcp_storage_hana_details.csv"
-  csv_input_file_name = "./development/gcp_server_hana_details.csv"
+  csv_input_file_name = "./development/gcp_storage_input_test_disks_linux.csv"
+}
+
+module "vm-creation" {
+  source  = "app.terraform.io/bso-cloud-03/vm-creation/google"
+  version = "1.0.0"
+  csv_input_file_name = "./development/gcp_storage_input_test_disks_linux.csv"
 }
